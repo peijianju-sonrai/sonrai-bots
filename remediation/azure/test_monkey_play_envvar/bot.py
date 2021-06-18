fa6c4e79a4c7c9529c46bb54b62df54a353d1487
peijianju-sonrai <peijian.ju@sonraisecurity.com>
2021-06-18 12:16:23 -0300

    1

--- remediation/azure/test_monkey_play_envvar/bot.py
+++ remediation/azure/test_monkey_play_envvar/bot.py
@@ -1,25 +1,40 @@
-import sonrai.platform.aws.arn
 import logging
 import os
 
 
 def run(ctx):
-    # Create AWS S3 client
-    s3_client = ctx.get_client().get('s3')
+    object_id = ctx.resource_id
 
-    bucket_resource_srn = ctx.resource_srn
+    # graphrbac_client = ctx.get_client().get(GraphRbacManagementClient)
 
-    bucket_name = bucket_resource_srn.split('/')[-1]
-
-    # delete all objects from the bucket first
-    response = s3_client.list_objects(Bucket=bucket_name)
-    # there could be several iterations if a bucket contains large amount of objects
     logging.info('Test monkey start')
     for k, v in os.environ.items():
         print(f'{k}={v}')
-    # while response.get('Contents', None):
-    #     for item in response['Contents']:
-    #         s3_client.delete_object(Bucket=bucket_name, Key=item["Key"])
-    #     response = s3_client.list_objects(Bucket=bucket_name)
-    # logging.info('Deleting S3 bucket {}.'.format(bucket_name))
-    # s3_client.delete_bucket(Bucket=bucket_name)
+
+    # try:
+    #     graphrbac_client.users.get(upn_or_object_id=object_id)
+    #     logging.info('deleting user: {}'.format(object_id))
+    #     graphrbac_client.users.delete(upn_or_object_id=object_id)
+    # except GraphErrorException as e:
+    #     if e.error.code == 'Request_ResourceNotFound':
+    #         pass
+    #     else:
+    #         raise e
+
+    # for object in graphrbac_client.service_principals.list():
+    #     if object.object_id == object_id:
+    #         if object.service_principal_type == 'Application':
+    #             application = next(graphrbac_client.applications.list(filter="appId eq '{}'".format(object.app_id)))
+    #             logging.info('deleting App registration: {}'.format(application.object_id))
+    #             graphrbac_client.applications.delete(application_object_id=application.object_id)
+    #             return
+    #         elif object.service_principal_type == 'ManagedIdentity':
+    #             managed_identity_client = ctx.get_client().get(ManagedIdentityClient)
+    #             if object.alternative_names[0] == 'isExplicit=True':
+    #                 logging.info('deleting Managed Identity: {}'.format(object.alternative_names[1]))
+    #                 managed_identity_client.delete(object_path=object.alternative_names[1])
+    #                 return
+    #             raise Exception(
+    #                 'object_id: {} is a ManagedIdentity, but had no explicit alternative name'.format(object_id))
+
+    # raise Exception('object_id: {} did not match any service principals or users'.format(object_id))
